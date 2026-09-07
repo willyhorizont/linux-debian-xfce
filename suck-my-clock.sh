e8b1c4f790b018b3c4a11e82b29b7d1aa50d0c38
willyhorizont <willyhorizont@gmail.com>
2026-09-08 01:37:46 +0700

    [Last updated: 08 Sep 2026 @ 01:37 AM][version: 0.0.9] update suck-my-clock.sh;

--- suck-my-clock.sh
+++ suck-my-clock.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-LABEL_TEXT=$($HOME/Codes/linux/SuckMyClock.sh)
+LABEL_TEXT=$($HOME/Codes/linux/SuckMyClock.pl)
+# LABEL_TEXT=$($HOME/Codes/linux/SuckMyClock.sh)
+# LABEL_TEXT=$($HOME/Codes/linux/SuckMyClock.py)
 
 echo -e "<txt><span font_family='monospace, Courier New' foreground='#FF1493'>$LABEL_TEXT</span></txt>\n<tool>Suck My Clock</tool>"
